ca781885d9d19c226f5a0fc3c1e3efcb54299370
rlagusals1102 <rlagusals1102@gmail.com>
2023-03-07 13:06:28 +0900

    기능 추가

--- WebView.py
+++ WebView.py
@@ -1,34 +1,35 @@
+#초안 완성
+
 import sys
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5 import QtCore
 
 class WebPage(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title = 'Web Page Viewer'
-        self.left = 100
-        self.top = 100
-        self.width = 800
-        self.height = 600
+        self.title = 'Web Page Viewer'  # 창 제목
+        self.left = 100  # 창 왼쪽 위치
+        self.top = 100  # 창 위쪽 위치
+        self.width = 800  # 창 너비
+        self.height = 600  # 창 높이
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # 웹 페이지 뷰어
+        # 웹 페이지 뷰어 생성
         self.web_view = QWebEngineView(self)
         self.web_view.load(QUrl('https://www.google.com/?hl=ko'))
 
-        # URL 입력 필드
+        # URL 입력 필드 생성 및 이벤트 처리
         self.url_input = QLineEdit()
         self.url_input.returnPressed.connect(self.load_url)
         self.url_input.setFixedHeight(30)
 
-        # 로드 버튼
+        # 로드 버튼 생성 및 이벤트 처리
         self.load_button = QPushButton('Load', self)
         self.load_button.setStyleSheet('''
             QPushButton {
@@ -43,21 +44,7 @@ class WebPage(QMainWindow):
         toolbar = QToolBar('Load')
         toolbar.addWidget(self.load_button)
 
-        # 새 창 버튼
-        self.new_window_button = QPushButton('New Window', self)
-        self.new_window_button.setStyleSheet('''
-            QPushButton {
-                border: none;
-                background-color: transparent;
-            }
-            QPushButton:hover {
-                background-color: #F5F5F5;
-            }
-        ''')
-        self.new_window_button.clicked.connect(self.new_window)
-        toolbar.addWidget(self.new_window_button)
-
-        # 전체 레이아웃
+        # 전체 레이아웃 생성
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
         hbox.addWidget(self.url_input)
@@ -75,15 +62,6 @@ class WebPage(QMainWindow):
         url = self.url_input.text()
         self.web_view.load(QUrl(url))
 
-    def keyPressEvent(self, event):
-        if event.modifiers() == QtCore.Qt.ControlModifier and event.key() == QtCore.Qt.Key_R:
-            self.web_view.reload()
-        else:
-            super().keyPressEvent(event)
-
-    def new_window(self):
-        new_window = WebPage()
-        new_window.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
