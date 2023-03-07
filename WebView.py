1b1650606db19f95e267d2a824e548d947c397ee
rlagusals1102 <rlagusals1102@gmail.com>
2023-03-07 13:01:43 +0900

    PyQt5 GUI 이용하여 Webview 구현

--- WebView.py
+++ WebView.py
@@ -1,73 +1,30 @@
 import sys
-import webbrowser
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-class WebSurf(QWidget):
+class WebPage(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title = 'Web Surfing'
+        self.title = 'Web Page Viewer'
         self.left = 100
         self.top = 100
-        self.width = 400
-        self.height = 150
+        self.width = 800
+        self.height = 600
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # URL 입력 필드
-        self.url_input = QLineEdit(self)
-        self.url_input.move(20, 20)
-        self.url_input.resize(260, 30)
-
-        # 검색 버튼
-        self.search_button = QPushButton('Search', self)
-        self.search_button.move(290, 20)
-        self.search_button.resize(80, 30)
-        self.search_button.clicked.connect(self.search)
-
-        # 구글 검색 버튼
-        self.google_button = QPushButton('Google', self)
-        self.google_button.move(20, 60)
-        self.google_button.resize(80, 30)
-        self.google_button.clicked.connect(self.google_search)
-
-        # 네이버 검색 버튼
-        self.naver_button = QPushButton('Naver', self)
-        self.naver_button.move(110, 60)
-        self.naver_button.resize(80, 30)
-        self.naver_button.clicked.connect(self.naver_search)
-
-        # 다음 검색 버튼
-        self.daum_button = QPushButton('Daum', self)
-        self.daum_button.move(200, 60)
-        self.daum_button.resize(80, 30)
-        self.daum_button.clicked.connect(self.daum_search)
+        # 웹 페이지 뷰어
+        self.web_view = QWebEngineView(self)
+        self.web_view.load(QUrl('https://www.google.com'))
+        self.setCentralWidget(self.web_view)
 
         self.show()
 
-    def search(self):
-        url = self.url_input.text()
-        webbrowser.open(url)
-
-    def google_search(self):
-        keyword = self.url_input.text()
-        url = 'https://www.google.com/search?q=' + keyword
-        webbrowser.open(url)
-
-    def naver_search(self):
-        keyword = self.url_input.text()
-        url = 'https://search.naver.com/search.naver?query=' + keyword
-        webbrowser.open(url)
-
-    def daum_search(self):
-        keyword = self.url_input.text()
-        url = 'https://search.daum.net/search?q=' + keyword
-        webbrowser.open(url)
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = WebSurf()
+    ex = WebPage()
     sys.exit(app.exec_())
